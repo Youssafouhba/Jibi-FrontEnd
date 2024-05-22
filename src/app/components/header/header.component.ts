0293247b1855304f003617e22a24d94b1686ffd7
Youssafouhba <youssefouhba@gmail.com>
2024-05-22 03:01:05 +0100

    loginpage and homa page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {LoginComponent} from "../login/login.component";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private logincomponent: LoginComponent) {
+  }
+
+  OpenLoginDialog() {
+    this.logincomponent.LoginDialog()
+  }
 
 }
